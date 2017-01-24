1b8efb01a84a8a60f1f2494ed4c6d7ac3778349a
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-01-24 00:26:31 -0400

    Se define la clase Tarifa

--- Tarea2/tarifa.py
+++ Tarea2/tarifa.py
@@ -4,8 +4,15 @@ Created on Jan 23, 2017
 @author: Alessandra Marrero 12-11091
 @author: Julio Fuenmayor 13-10488
 '''
+from datetime import datetime
+from time import strptime
 
-
+class Tarifa:
+    def __init__(self, weekDay = 1, weekendDays = 2):
+        self.semana = weekDay
+        self.finDeSemana = weekendDays
+    
+        
 def calcularPrecio(tarifa, tiempoDeServicio):
     
     
@@ -14,5 +21,9 @@ def calcularPrecio(tarifa, tiempoDeServicio):
 
 
 if __name__ == '__main__':
+    tarifa = Tarifa()
+    
+    print("La tarifa de los dias de semana es: %d")%(tarifa.semana)
+    print("La tarifa de los dias de fin de semana es: %d")%(tarifa.finDeSemana)
     
     
