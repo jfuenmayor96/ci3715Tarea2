16aa3b23d6261566a36563cb1e1ce2feef0eeedd
ale <ale@ale-VPCEG20EL>
2017-01-26 16:33:57 -0400

    Prueba 1

---
+++ Tarea2/TestSuite.py
@@ -0,0 +1,22 @@
+'''
+Created on Jan 26, 2017
+
+@author: Alessandra Marrero 12-11091
+@author: Julio Fuenmayor 13-10488
+'''
+import unittest
+from tarifa import *
+
+
+class TestSuite(unittest.TestCase):
+
+    '''
+    Este es el caso de prueba de frontera donde se tiene un trabajo de
+    7 diass. 
+    ''' 
+    def testTiempoMax(self):
+        t = Tarifa([1,0],[1,0])
+        inicio = datetime(2000,1,1,8,0,0,0)
+        fin = datetime(2000,1,8,8,0,0,0)
+        servicio = [inicio, fin]
+        self.assertEqual(calcularPrecio(t,servicio),168.0 , "La prueba ha fallado")
